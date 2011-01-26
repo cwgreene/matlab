f619c61b828fad736bb9ac5eee4f53e7d7a80ad0
Chris <archgoon@gmail.com>
2011-01-25 16:21:09 -0800

    Documentation comment.

--- adjlist2matrix.m
+++ adjlist2matrix.m
@@ -1,3 +1,12 @@
+%takes in a list of matrices, x = {[1,2,3],[2,3,4]}}.
+%disconnected points need to show up as empty lists,
+%assume that nodes are numbered in sane way.
+
+%Input: adjlists is a list of N vectors, whose entries
+%       are integers with values 1 to N. Repeat values
+%       are allowed, indicating multiple connections.
+%       Input is assumed to be directed.
+%Output: NxN adjacency matrix.
 function adjmatrix = adjlist2matrix(adjlists)
     adjmatrix = zeros(length(adjlists),length(adjlists));
     for i = 1:length(adjlists)
